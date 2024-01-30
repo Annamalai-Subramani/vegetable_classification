7efda9a6be8494404baa400515f179c72f325980
Annamalai-Subramani <annamalai008s@gmail.com>
2024-01-30 23:26:55 +0530

    app files updated

---
+++ app.py
@@ -0,0 +1,76 @@
+import os
+import json
+from PIL import Image
+
+import numpy as np
+import tensorflow as tf
+import streamlit as st
+
+import streamlit as st
+
+# Your Streamlit app code
+
+
+
+# Run your Streamlit app
+
+
+
+model_path = 'D:/Projects/vegetable_classification/vegetable_classification/trained_model/vegetable_prediction_resnet_model.h5'
+# Load the pre-trained model
+model = tf.keras.models.load_model(model_path)
+
+# loading the class names
+class_indices_vegetables = json.load(open('D:/Projects/vegetable_classification/vegetable_classification/class_indices_vegetables.json'))
+
+
+# Function to Load and Preprocess the Image using Pillow
+def load_and_preprocess_image(image_path, target_size=(224, 224)):
+    # Load the image
+    img = Image.open(image_path)
+    # Resize the image
+    img = img.resize(target_size)
+    # Convert the image to a numpy array
+    img_array = np.array(img)
+    # Add batch dimension
+    img_array = np.expand_dims(img_array, axis=0)
+    # Scale the image values to [0, 1]
+    img_array = img_array.astype('float32') / 255.
+    return img_array
+
+# Function to Predict the Class of an Image
+def predict_image_class(model, image_path, class_indices):
+    preprocessed_img = load_and_preprocess_image(image_path)
+    predictions = model.predict(preprocessed_img)
+
+    # Get the predicted class index
+    predicted_class_index = np.argmax(predictions, axis=1)[0]
+
+    # Adjust for the 1-indexed class indices
+    predicted_class_name = list(class_indices.keys())[predicted_class_index]
+
+    return predicted_class_name
+
+
+# Streamlit App
+st.title('Vegetable Prediction')
+
+uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+
+
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    col1, col2 = st.columns(2)
+
+    with col1:
+        resized_img = image.resize((150, 150))
+        st.image(resized_img)
+
+    with col2:
+        if st.button('Classify'):
+            prediction = predict_image_class(model, uploaded_image, class_indices_vegetables)
+            st.markdown(f'<div style="font-size: 24px; color: black;">Predicted Vegetable: {prediction}</div>', unsafe_allow_html=True)
+
+            
+            
+   
